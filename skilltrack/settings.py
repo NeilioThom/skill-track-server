2e5e9b4c20601105ff5cfe192f7359f59b0500a9
= <neil878@gmail.com>
2018-03-08 12:07:33 +0000

    Added support for differing server environments to ease deployment

--- skilltrack/settings.py
+++ skilltrack/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com', '.10k-hours.io']
 
