b4bc50561655322cd3158f74e56ecfefc74a2c94
= <neil878@gmail.com>
2018-03-08 12:29:22 +0000

    Fixes for static files support in prod

--- skilltrack/settings.py
+++ skilltrack/settings.py
@@ -12,8 +12,8 @@ ENVIRON = os.environ.get('ENVIRON')
 
 DEBUG = False
 
-#if ENVIRON == 'DEBUG':
- #   DEBUG = True
+if ENVIRON == 'DEBUG':
+   DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com', '.10k-hours.io']
 
@@ -123,12 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/staticfiles/'
 
 # Debug settings (Cors)
 if DEBUG:
